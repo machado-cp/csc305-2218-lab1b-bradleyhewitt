043ecc66ab82f0eae19554f9cf8e1aa1d8297689
Bradley Hewitt <40677305+bradleyhewitt@users.noreply.github.com>
2021-09-21 14:42:11 -0700

    Update DebuggerMain.java

--- src/edu/calpoly/csc305/debugging/DebuggerMain.java
+++ src/edu/calpoly/csc305/debugging/DebuggerMain.java
@@ -1,6 +1,10 @@
 package edu.calpoly.csc305.debugging;
 
 public class DebuggerMain {
+  /** Debug Thesaurus.
+   *
+   * @param args // Command-line args
+   */
   public static void main(String[] args) {
     Thesaurus thesaurus = new Thesaurus();
 

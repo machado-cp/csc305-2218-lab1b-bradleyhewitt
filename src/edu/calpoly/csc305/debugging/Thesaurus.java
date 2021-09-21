386654670eaa925df50fecef745b55f347876efd
Bradley Hewitt <40677305+bradleyhewitt@users.noreply.github.com>
2021-09-21 14:43:06 -0700

    Update Thesaurus.java

--- src/edu/calpoly/csc305/debugging/Thesaurus.java
+++ src/edu/calpoly/csc305/debugging/Thesaurus.java
@@ -8,12 +8,24 @@ import java.util.Map;
 public class Thesaurus {
   private Map<String, List<String>> synonyms;
 
+  /** Create a mapping between word and synonyms.
+   *
+   */
   public Thesaurus() {
     synonyms = new HashMap<>();
   }
 
+  /** Add synonyms for a word.
+   *
+   * @param word // Word to be explored
+   * @param alternatives // Synonyms
+   */
   public void addAlternatives(String word, String ... alternatives) {
-    synonyms.get(word).addAll(Arrays.asList(alternatives));
+    if (!synonyms.containsKey(word)) {
+      synonyms.put(word, Arrays.asList(alternatives));
+    } else {
+      synonyms.get(word).addAll(Arrays.asList(alternatives));
+    }
   }
 
   public List<String> alternatives(String word) {
